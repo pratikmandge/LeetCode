3686af0bf98ed5cb61c87ce21ff4b312de58ea6c
Pratik Mandge <88229260+pratikmandge@users.noreply.github.com>
2024-08-06 10:20:22 +0530

    Create 1920. Build Array from Permutation.py

---
+++ 1920. Build Array from Permutation.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/build-array-from-permutation/
+
+
+def buildArray(nums: list[int]) -> list[int]:
+    answer = []
+    for i in range(len(nums)):
+        answer.append(nums[nums[i]])
+    return answer
+
+
+# Example 1:
+# Input:
+nums = [0, 2, 1, 5, 3, 4]
+print(buildArray(nums))
+# Output: [0,1,2,4,5,3]
+# Explanation: The array ans is built as follows:
+# ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+#     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+#     = [0,1,2,4,5,3]
+
+
+# Example 2:
+# Input:
+nums = [5, 0, 1, 2, 3, 4]
+print(buildArray(nums))
+# Output: [4,5,0,1,2,3]
+# Explanation: The array ans is built as follows:
+# ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+#     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+#     = [4,5,0,1,2,3]
